cb68199cf6df395dc28232afa32d727499f8f2e8
dp3698 <dp3698@student.uni-lj.si>
2013-11-28 16:56:31 +0100

    [Update]: Camera projection first look
    - It could happen that both parallel (orthographic) and perspective
    projection will be in same *.html … only of course in separate tags …

--- src/scripts/camera-projection.js
+++ src/scripts/camera-projection.js
@@ -1,6 +1,81 @@
+var WorldAXES, CameraAXES, ObjectAXES;
+var plane,     camera,     object;
+
+var CameraFrustom;
+
+var cube; /*For easy access*/
+
 function exampleInit() {
+	controls.noZoom = true;
+	controls.noPan = true;
+	controls.noRoll = true;
+	controls.staticMoving = true;
+	
+	controls.target.set(100,250,100);
+	GCamera.position.set(300,300,300);
+	
+	/******* 
+	  AXES 
+	*******/
+	WorldAXES = new THREE.AxisHelper( 100 );
+	CameraAXES = new THREE.AxisHelper( 100 );
+	ObjectAXES = new THREE.AxisHelper( 100 );
+	
+	GScene.add( WorldAXES );
+	
+	/********* 
+	  OBJECT 
+	**********/
+	object = new THREE.Object3D();
+	
+	cube = new THREE.Mesh( 
+		new THREE.CubeGeometry( 20, 20, 20, 1, 1, 1 ), 
+		new THREE.MeshBasicMaterial( { color : 0xff0000, wireframe: true } )
+	); cube.position.set(10,10,10);
+	
+	object.add(cube);
+	object.add(ObjectAXES);
+	object.position.set(200,250,100);
+	
+	GScene.add( object );
+	
+	/********* 
+	  CAMERA 
+	**********/
+	if(false) camera = new THREE.OrthographicCamera( -10, 10, 10, -10, 150, 250 );
+	else camera = new THREE.PerspectiveCamera( 30, 20/20, 1, 250 );
+	camera.position.set(-200,250,100);
+	camera.lookAt(object.position);
+	camera.updateProjectionMatrix();
+	
+	CameraFrustom = new CameraHelper(camera);
+	camera.add(CameraFrustom);
+	camera.add(CameraAXES);
+	
+	
+	GScene.add( camera );
+}
+
+function exampleRender() {
+	CameraFrustom.update();
+}
+
+// adding new vector to the list
+function addVectorToList(vec) {
+    vectors.push(vec);
+	
+    createVectorSelection(vec, vectors.length-1);
+	
+	updateVectorDropDownLists(vectors.length - 1);	
+}
+
+/****************
+   CALCULATION
+*****************/
+
+
 	// Cube
-	var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+	/*var geometry = new THREE.CubeGeometry( 200, 200, 200 );
 
 	for ( var i = 0; i < geometry.faces.length; i += 2 ) {
 
@@ -24,22 +99,4 @@ function exampleInit() {
 	var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
 
 	plane = new THREE.Mesh( geometry, material );
-	scene.add( plane );
-}
-
-function exampleRender() {
-	plane.rotation.y = cube.rotation.y += ( targetRotation - cube.rotation.y ) * 0.05; //Easing effect
-}
-
-// adding new vector to the list
-function addVectorToList(vec) {
-    vectors.push(vec);
-	
-    createVectorSelection(vec, vectors.length-1);
-	
-	updateVectorDropDownLists(vectors.length - 1);	
-}
-
-/****************
-   CALCULATION
-*****************/
+	scene.add( plane );*/
