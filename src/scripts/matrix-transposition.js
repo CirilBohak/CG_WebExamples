1e9098df4118ce0606cc77365e127ca094a287d3
Nejc Štebe <nejc.stebe92@gmail.com>
2014-11-02 18:49:28 +0100

    Finished matrix transposition.

--- src/scripts/matrix-transposition.js
+++ src/scripts/matrix-transposition.js
@@ -1,8 +1,9 @@
 enabledAddingPoints = false;
 
-var matrix01;
+var matrix01, matrix02;
 var n = Math.floor(Math.random() * 5) + 2, 
 	m = Math.floor(Math.random() * 5) + 2;
+var transposed = false;
 
 function exampleInit() {
 	// Matrix constructor takes dimensions parameters in another
@@ -19,6 +20,14 @@ function bindEvents() {
 		updateMatrixVariable(Number($(this).parent().attr("class")[1]),
 							 Number($(this).parent().attr("class")[2]),
 							 Number($(this).val()));
+	});
+
+	$("#transpose").click(function() {
+		if (!transposed) {
+			transposed = true;
+			createMatrix("#matrix02", m, n);
+			updateMatrix("#matrix02", m, n, matrix01.transpose().get1D_array());
+		}
 	});
 }
 
